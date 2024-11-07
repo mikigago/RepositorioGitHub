d3d2633a996de1b903c7fa6066af4502ce220af2
Gago9 <Gago9@Miguel>
2024-11-07 14:06:35 +0100

    Golf

---
+++ RepositorioGitHub/src/PracticaGolf/PracticaGolf.java
@@ -0,0 +1,80 @@
+package PracticaGolf;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class PracticaGolf {
+
+	public static void main(String[] args) {
+
+		int golpesJugador = 0;
+		int numeroHoyo = 1;
+		int puntuacionTotal = 0;
+		int puntuacionHoyo = 0;
+
+		char jugadorSeRetira = ' ';
+
+		String clasificacionPuntos = "";
+		String clasificacionCompleta = "";
+		String Albatros = "Albatros", Eagle = "Eagle", Birdie = "Birdie", Par = "Par", Bogey = "Bogey",
+				DoubleBogey = "Double Bogey", TripleBogey = "Triple Bogey";
+
+		Scanner entrada = new Scanner(System.in);
+
+		do {
+
+			System.out.println("Introduce el número de golpes realizado en el hoyo número " + numeroHoyo + " :");
+			golpesJugador = entrada.nextInt();
+
+			if (golpesJugador > 7 || golpesJugador < 1) {
+				System.out.println("--Número de golpes nulo, vuelve a introducirlo--");
+			} else {
+				puntuacionHoyo = golpesJugador - 4;
+				puntuacionTotal += puntuacionHoyo;
+				numeroHoyo++;
+			}
+
+			if (puntuacionHoyo == 0) {
+				clasificacionPuntos = Par;
+			} else if (puntuacionHoyo == 1) {
+				clasificacionPuntos = Bogey;
+			} else if (puntuacionHoyo == 2) {
+				clasificacionPuntos = DoubleBogey;
+			} else if (puntuacionHoyo == 3) {
+				clasificacionPuntos = TripleBogey;
+			} else if (puntuacionHoyo == -1) {
+				clasificacionPuntos = Birdie;
+			} else if (puntuacionHoyo == -2) {
+				clasificacionPuntos = Eagle;
+			} else if (puntuacionHoyo == -3) {
+				clasificacionPuntos = Albatros;
+			} else {
+				clasificacionPuntos = "Sin clasificación";
+			}
+
+			if (!clasificacionCompleta.isEmpty()) {
+				clasificacionCompleta = clasificacionCompleta.concat(", ");
+
+			} else {
+				clasificacionCompleta = clasificacionCompleta + clasificacionPuntos;
+			}
+
+			System.out.println("¿Quieres ir al siguiente hoyo? (S/N)");
+			jugadorSeRetira = entrada.next().charAt(0);
+			entrada.nextLine();
+
+			if (jugadorSeRetira == 'N' || jugadorSeRetira == 'n') {
+				break;
+			}
+
+		} while (numeroHoyo < 18);
+
+		System.out.println("------------------------------PUNUACIONES---------------------------");
+		System.out.println("Número total de golpes: " + (puntuacionTotal + 4 * (numeroHoyo - 1)));
+		System.out.println("Puntuación total: " + puntuacionTotal);
+		System.out.println("Clasificación por puntos: " + clasificacionCompleta);
+		System.out.println("¡Gracias por su participación!");
+		System.out.println("--------------------------------------------------------------------");
+		entrada.close();
+	}// class
+}// main
